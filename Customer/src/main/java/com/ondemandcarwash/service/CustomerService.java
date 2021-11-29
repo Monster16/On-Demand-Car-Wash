2e0cdf033bf929e566c7ee3b45906b022bed36e2
anilysin <anilysin@LIN25007647.corp.capgemini.com>
2021-11-30 01:26:44 +0530

    COMMITING FIRST TIME AND HALF DONE WITH CUSTOMER MICROSERVICE

---
+++ Customer/src/main/java/com/ondemandcarwash/service/CustomerService.java
@@ -0,0 +1,38 @@
+package com.ondemandcarwash.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ondemandcarwash.model.Customer;
+import com.ondemandcarwash.repository.CustomerRepository;
+
+@Service
+public class CustomerService {
+
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	
+	
+	public Customer addCustomer(Customer customer) {
+		return customerRepository.save(customer);
+		
+	}
+
+	public List<Customer> getCustomers() {
+		List<Customer> customers =customerRepository.findAll();
+		System.out.println("Getting Customers from DB" + customers);
+		return customers;
+	}
+
+	public void deleteById(int id) {
+		customerRepository.deleteById(id);
+		
+	}
+	
+	public void deleteUser(Customer customer) {
+		customerRepository.delete(customer);
+	}
+}
